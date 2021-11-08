3461908c90cc52b30c87ce754969d06a59da5cb6
Manish Goyal <83345472+manishgoyal30@users.noreply.github.com>
2021-11-08 16:28:44 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+let curDate = new Date();
+curDate = curDate.getHours();
+let greeting = '';
+const cssStyle = {};
+
+if(curDate >=1 && curDate <12){
+  greeting = "Good Morning";
+  cssStyle.color= "green";
+}
+else if(curDate >=12 && curDate <19){
+  greeting = "Good Afternoon";
+  cssStyle.color= "orange";
+}
+else{
+  greeting = "Good Evening";
+  cssStyle.color = "neon";
+ 
+}
+
+ReactDOM.render(
+  <>
+
+  <h1 className="heading">Hello Sir, <span style={cssStyle}>{greeting}</span></h1>
+  </>, 
+  document.getElementById('root')
+);
+
+
